bc4190e164eb3f70ebcf9642513753894f447a5a
Jason Schrader <whoabuddy@users.noreply.github.com>
2020-12-14 09:15:27 -0700

    update js and add lite-yt-embed

--- static/js/toggles.js
+++ static/js/toggles.js
@@ -25,36 +25,3 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
-
-/*
-
-// light and dark mode toggle
-
-const lightswitch = document.querySelector(".ux__header__icon--lightswitch");
-const currentTheme = localStorage.getItem("theme")
-
-if (currentTheme == "dark") {
-  document.documentElement.classList.add("darkness");
-}
-
-lightswitch.addEventListener("click", function() {
-  
-  document.documentElement.classList.toggle("darkness");
-  
-  let theme = "light";
-  if (document.documentElement.classList.contains("darkness")) {
-    theme = "dark";
-  }
-  localStorage.setItem("theme", theme);
-
-});
-
-// menu toggle 
-
-const menubtn = document.querySelector(".ux__header__icon--menu");
-const menu = document.querySelector(".ux__header__nav");
-
-menubtn.addEventListener("click", function() {
-  menu.classList.toggle("show");
-});
-*/
